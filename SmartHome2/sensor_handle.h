40da9f86a265797b647452bb703cb6a5b70ddf07
Nguyen Duc Hanh <138410075+ndhanh2711@users.noreply.github.com>
2025-03-09 02:47:54 +0700

    Update sensor_handle.h

--- SmartHome2/sensor_handle.h
+++ SmartHome2/sensor_handle.h
@@ -1,5 +1,10 @@
 #ifndef SENSOR_HANDLE_H
 #define SENSOR_HANDLE_H
+#include "DHT.h"
+
+extern DHT dht1;
+extern DHT dht2;
+extern DHT dht3;
 
 // Khai báo các chân cảm biến
 #define sensorPin1  25
@@ -10,6 +15,15 @@
 #define Flame2_PIN 35
 #define Flame3_PIN 36
 
+// Khai báo chân kết nối với cảm biến DHT11
+#define DHTPIN1 13
+#define DHTPIN2 12
+#define DHTPIN3 14
+
+// Loại cảm biến (DHT11)
+#define DHTTYPE DHT11
+
+
 // Khai báo biến toàn cục (sử dụng extern)
 extern int Flame1_Value;
 extern int Flame2_Value;
@@ -19,6 +33,14 @@ extern int CO1_Value;
 extern int CO2_Value;
 extern int CO3_Value;
 
+extern int Temp1_Value;
+extern int Temp2_Value;
+extern int Temp3_Value;
+
+extern int Hum1_Value;
+extern int Hum2_Value;
+extern int Hum3_Value;
+
 // Khai báo các hàm
 int MQ21_getValue();
 int MQ22_getValue();
@@ -27,5 +49,13 @@ int MQ23_getValue();
 int flame1_getValue();
 int flame2_getValue();
 int flame3_getValue();
+
+int temp1_getValue();
+int temp2_getValue();
+int temp3_getValue();
+
+int hum1_getValue();
+int hum2_getValue();
+int hum3_getValue();
 
 #endif
